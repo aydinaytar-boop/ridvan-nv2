dbd98fe2b1b813b84aa13d3d42d71ae45802da46
aydinaytar-boop <aydin.aytar@gmail.com>
2026-04-11 17:55:33 +0200

    all

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ridvan-nv2/",   // <<< GİTHUB PAGES İÇİN ZORUNLU SATIR
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
