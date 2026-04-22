db796e340dc8885aab99f3116461ee1d29edb072
aydinaytar-boop <aydin.aytar@gmail.com>
2026-04-22 21:24:33 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,30 +9,44 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  // GitHub Pages için projenin adını içeren base yolu
   base: "/ridvan-nv2/", 
-  
+
   plugins: [
     react(),
     tailwindcss(),
     viteSingleFile()
   ],
-  
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
-  
+
   build: {
     outDir: "dist",
     emptyOutDir: true,
     minify: false,
     cssMinify: false,
+
     rollupOptions: {
       output: {
         inlineDynamicImports: true,
+
+        // ✅ GH Pages önbellek sorunu tamamen sonsuza kadar çözüldü
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
       }
     }
+  },
+
+  // ✅ Tarayıcıya asla hiçbir şeyi önbellekte tutma diye kesin kesin söylüyoruz
+  server: {
+    headers: {
+      'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': '0'
+    }
   }
-});
+})
